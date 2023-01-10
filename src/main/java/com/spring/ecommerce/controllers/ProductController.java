0bee8e8aa5e5f0c035d0154335904e62ae75848b
Sole Rocha <soledadrocha@outlook.com.ar>
2023-01-09 23:22:18 -0300

    Create of method DELETE and modified show.html

--- src/main/java/com/spring/ecommerce/controllers/ProductController.java
+++ src/main/java/com/spring/ecommerce/controllers/ProductController.java
@@ -62,4 +62,10 @@ public class ProductController {
         return "redirect:/products";
     }
 
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+        productService.delete(id);
+        return "redirect:/products";
+    }
+
 }
